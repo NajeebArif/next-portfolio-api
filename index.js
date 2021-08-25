1b15f762a5330e9b1e501971f9cd470cbba3859d
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-25 15:44:50 +0530

    Update index.js
    Connect to MongoDb Atlas

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 const express = require('express')
 const server = express();
 const PORT = parseInt(process.env.PORT, 10) || 3001;
+const mongoose = require('mongoose')
+const config = require('./config/dev');
+
+mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, (err) => {
+    if (err) { console.error(err); }
+    else {
+        console.log('Connected to DB!');
+    }
+})
 
 server.use(express.json());
 
