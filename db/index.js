f051f61b1067a697e06d8eb6b81132a13b521c7e
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-25 17:13:19 +0530

    Update index.js
    Register portfolio model with mongoose

--- db/index.js
+++ db/index.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose')
 const config = require('../config/dev');
 
+require('./models/portfolio')
+
 exports.connect = () => {
     return new Promise((resolve, reject) =>{
         mongoose.connect(config.DB_URI, {
