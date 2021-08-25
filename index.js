8b3c37a4bef298c12f4220fb77d7f8198f3bb4e7
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-25 16:13:45 +0530

    Update index.js
    Syncronise the db connection and then the server starting

--- index.js
+++ index.js
@@ -3,26 +3,22 @@
 const express = require('express')
 const server = express();
 const PORT = parseInt(process.env.PORT, 10) || 3001;
-const mongoose = require('mongoose')
-const config = require('./config/dev');
+const { promisify } = require('util');
 
-mongoose.connect(config.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, (err) => {
-    if (err) { console.error(err); }
-    else {
-        console.log('Connected to DB!');
-    }
-})
+async function runServer() {
+    
+    const isDbConnected = await require('./db').connect();
+    console.log(isDbConnected)
 
-server.use(express.json());
+    server.use(express.json());
+    server.use('/api/v1/portfolios', require('./routes/portfolioRoutes'));
 
-server.use('/api/v1/portfolios', require('./routes/portfolioRoutes'));
+    server.listen(PORT, err => {
+        if (err)
+            console.error(err)
+        else
+            console.info(`Server started on port ${PORT}`)
+    })
+}
 
-server.listen(PORT, err => {
-    if (err)
-        console.error(err)
-    else
-        console.info(`Server started on port ${PORT}`)
-})
+runServer();
