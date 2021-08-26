51c9c83b43da729a05d514188b6b03828d2bec1a
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-26 14:39:44 +0530

    Add create POrtfolio controller function and router
    Add create POrtfolio controller function and router

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -2,16 +2,22 @@
 const mongoose = require('mongoose')
 const Portfolio = mongoose.model('Portfolio')
 
-exports.getPortfolios = async (req, res)=>{
+exports.getPortfolios = async (req, res) => {
     const portfolios = await Portfolio.find({})
     return res.json(portfolios)
 }
 
 exports.getPortfolioById = async (req, res) => {
-    try{
+    try {
         const portfolio = await Portfolio.findById(req.params.id);
         return res.json(portfolio)
-    }catch (err){
+    } catch (err) {
         return res.status(422).send(err.message);
     }
+}
+
+exports.createPortfolio = async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    return res.json({ message: 'Creating Portfolio...' });
 }
--- routes/portfolioRoutes.js
+++ routes/portfolioRoutes.js
@@ -2,9 +2,10 @@
 const express = require('express')
 const router = express.Router();
 
-const { getPortfolios, getPortfolioById } = require('../controllers/portfolioController')
+const { getPortfolios, getPortfolioById, createPortfolio } = require('../controllers/portfolioController')
 
 router.get('', getPortfolios)
 router.get('/:id', getPortfolioById)
+router.post('', createPortfolio);
 
 module.exports = router;
