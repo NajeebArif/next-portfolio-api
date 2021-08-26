ffdf911229560585af06d7a34d48540be28ee05d
Najeeb Arif <NajeebArif786@outlook.com>
2021-08-26 16:27:02 +0530

    Update auth.js
    Add jwt authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,13 +6,13 @@ const jwksRsa = require('jwks-rsa');
 // of a request
 // It will verify access token against Auth0 JSON web key set
 exports.checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 10,
-    jwksUri: 'https://dev-rloaycly.us.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'https://dev-rloaycly.us.auth0.com/api/v2/',
-  issuer: 'https://dev-rloaycly.us.auth0.com/',
-  algorithms: ['RS256']
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-rloaycly.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'custom_portfolio_api',
+    issuer: 'https://dev-rloaycly.us.auth0.com/',
+    algorithms: ['RS256']
 });
